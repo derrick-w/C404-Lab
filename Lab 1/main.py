db8674c06c226d8a44f1752a6a328710ac6a8eb7
dwai <dwai@ualberta.ca>
2019-01-10 17:57:29 -0700

    Requests my python file

--- Lab 1/main.py
+++ Lab 1/main.py
@@ -4,9 +4,11 @@ import requests
 
 print(requests.__version__)
 
-r = requests.get("http://www.google.com")
+r = requests.get("https://raw.githubusercontent.com/derrick-w/C404-Lab/master/Lab%201/main.py")
+
 #print(r)
 
-# print(dir(r))
+#print(dir(r))
 print(r.text)
-print(r.status_code)
+#print(r.status_code)
+
